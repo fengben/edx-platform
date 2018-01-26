e0f6b3bf5eac0442c219033b1615220220ab67a0
Jesse Zoldak <zoldak@edx.org>
2018-01-26 15:20:42 -0500

    [fixup]

--- scripts/jenkins-quality-diff.sh
+++ scripts/jenkins-quality-diff.sh
@@ -14,20 +14,4 @@ echo "Running diff quality."
 mkdir -p test_root/log/
 LOG_PREFIX=test_root/log/run_quality
 
-# we expect the pylint results for each system in REPO_ROOT/reports
-# e.g. REPO_ROOT/reports/pavelib/pylint.report
-if [[ -s reports/pavelib/pylint.report ]]; then
-    echo 'Found pylint results: reports/pavelib/pylint.report'
-else
-    echo 'ERROR: could not find reports/pavelib/pylint.report'
-    if [[ -d reports/pavelib ]]; then
-        echo 'Found the directory: reports/pavelib'
-    else
-        echo 'ERROR: could not find the reports/pavelib directory'
-    fi
-    echo 'ls * yields:'
-    ls *
-    exit 1
-fi
-
 paver run_quality -p 100 -l $LOWER_PYLINT_THRESHOLD:$UPPER_PYLINT_THRESHOLD 2> $LOG_PREFIX.err.log > $LOG_PREFIX.out.log
